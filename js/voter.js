995435db45741e1701fe4f52479aed5e5a6c05e6
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2012-11-25 14:15:28 -0500

    Add sorta-working RMS extraction
    - Use raw buffers instead of dealing with wav extraction
    - Update and move recorder / recorderWorker files
    - Refactor all sample functions in its own class.

--- js/voter.js
+++ js/voter.js
@@ -1,70 +1,81 @@
-n = navigator;
 function onSuccess(stream) {
   var context = new webkitAudioContext();
   var mediaStreamSource = context.createMediaStreamSource(stream);
-  var rec = new Recorder(mediaStreamSource);
+  var rec = new Recorder(mediaStreamSource, { workerPath: '/js/recorderWorker.js'});
   rec.record();
 
   intervalKey = setInterval(function() {
-    rec.exportWAV(function(blob) {
-      reader = new FileReader();
-      reader.onloadend = function() {
-        var arr = to_mono(new Int16Array(this.result));
-        var res = rms(arr);
-        // @TODO : A-weighting filter. Ressources : 
-        // - algorithm : https://gist.github.com/148112
-        // - convolve : re-implement from numpy in js
-        //              http://docs.scipy.org/doc/numpy/reference/generated/numpy.convolve.html
-        console.log(Math.log(res));
-      }
-      reader.readAsArrayBuffer(blob)
+    rec.getBuffer(function(buffers) {
+      var sample = Sample.from_stereo(buffers[0], buffers[1]);
+      console.log(sample.rms().toFixed(2));
       rec.clear();
     });
   }, 1000);
 }
+
 
 function onError(e) {
   console.log('nope :(');
 }
 
 document.addEventListener("DOMContentLoaded", function(){
+
+
+
   document.querySelector('a').addEventListener('click', function() {
     navigator.webkitGetUserMedia({ audio: true }, onSuccess, onError);
   });
 }, false );
 
-function rms(sample) {
-  return Math.sqrt(Math.pow(mean(sample), 2) + sd_squared(sample));
+
+
+
+function Sample(buffer) {
+  this._buffer = buffer;
+  this._mean = null;
+  this._sd_squared = null;
+  this._sum = null;
 }
 
-function sd_squared(sample) {
-  var mean_calc = mean(sample),
-    total = 0;
-
-  for(var i = 0; i < sample.length; i++) {
-    total += Math.pow(sample[i] - mean_calc, 2);
+Sample.from_stereo = function(left, right) {
+  var size = left.length,
+      combined = new Float32Array(size);
+  for(var i = 0; i < size; i++) {
+    combined.set([left[i] / 2 + right[i] / 2], i );
   }
-
-  return total;
+  return new Sample(combined);
 }
 
-function mean(sample) {
-  return sum(sample) / sample.length
-}
+Sample.prototype = {
+  rms: function() {
+    return Math.sqrt(Math.pow(this.mean(), 2) + this.sd_squared());
+  },
 
-function to_mono(sample) {
-  var size = sample.length,
-      mono_sound = new Int16Array(size / 2);
-  for(var i = 0; i < size; i += 2) {
-    var left = sample[i],
-        right = sample[i+1];
-    mono_sound.set([left / 2 + right / 2], i / 2);
+  mean: function() {
+    if(this._mean === null) {
+      this._mean = this.sum() / this._buffer.length;
+    }
+    return this._mean;
+  },
+
+  sd_squared: function() {
+    if(this._sd_squared === null) {
+      var mean = this.mean();
+      for(var i = 0; i < this._buffer.length; i++) {
+        this._sd_squared += Math.pow(this._buffer[i] - mean, 2);
+      }
+    }
+
+    return this._sd_squared;
+  },
+
+  sum: function() {
+    if(this._sum === null) {
+      this._sum = 0;
+      for(var i = 0; i < this._buffer.length; i++) {
+        this._sum += this._buffer[i];
+      }
+    }
+    return this._sum;
   }
-  return mono_sound;
-}
-
-function sum(sample) {
-  var total = 0;
-  for(var i = 0; i < sample.length; i++) { total += sample[i]; }
-  return total;
-}
+};
