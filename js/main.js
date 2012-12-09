8951adc8053d6d0425caaea2404db40e0c2d0a48
Antoine Leclair <antoineleclair@gmail.com>
2012-12-09 14:06:29 -0500

    jquery => zepto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 requirejs.config({
   paths: {
-    jquery: '//code.jquery.com/jquery.min',
+    zepto: '//cdnjs.cloudflare.com/ajax/libs/zepto/1.0rc1/zepto.min',
     bootstrap: '//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min',
     underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min',
     backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min'
@@ -19,6 +19,6 @@ requirejs.config({
   }
 });
 
-require(['jquery', 'underscore', 'backbone', 'voter'], function($, _, Backbone, Voter) {
+require(['zepto', 'underscore', 'backbone', 'voter'], function($, _, Backbone, Voter) {
   window.voter = new Voter;
 });
