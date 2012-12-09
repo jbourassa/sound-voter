e7d1fc044f59ba59abd759d72069ff5872715338
Antoine Leclair <antoineleclair@gmail.com>
2012-12-09 17:47:52 -0500

    Adds recorder and sampler as dependencies.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ requirejs.config({
     bootstrap: '//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min',
     underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min',
     backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min',
-    highcharts: '//code.highcharts.com/highcharts'
+    highcharts: '//code.highcharts.com/highcharts',
+    recorder: '/js/vendor/recorder',
+    sampler: '/js/sampler'
   },
   shim: {
     underscore: {
@@ -19,11 +21,14 @@ requirejs.config({
     },
     bootstrap: ['jquery'],
     highcharts: [],
+    recorder: [],
+    sampler: ['recorder']
   }
 });
 
-require(['jquery', 'underscore', 'backbone', 'bootstrap', 'highcharts', 'voter'],
-  function($, _, Backbone, Bootstrap, HighCharts, Voter) {
+require(['jquery', 'underscore', 'backbone', 'bootstrap', 'highcharts',
+         'recorder', 'sampler', 'voter'],
+  function($, _, Backbone, Bootstrap, HighCharts, Recorder, Sampler, Voter) {
     window.voter = new Voter();
     
     Backbone.history.start({
