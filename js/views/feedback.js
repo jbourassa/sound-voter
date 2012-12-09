42a4a1b5f12daa1297a16ba3314ed99b0b9a18f2
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2012-12-09 17:40:14 -0500

    Add dummy highcharts graph.

--- js/views/feedback.js
+++ js/views/feedback.js
@@ -13,6 +13,42 @@ define(['jquery', 'backbone', 'underscore', 'text!templates/feedback.html'],
         this.$el.html(html);
         this.$el.modal();
         
+        this.chart = new Highcharts.Chart({
+          chart: {
+            renderTo: this.$el.find('.chart')[0],
+            type: 'area',
+            marginRight: 10
+          },
+          title: {
+            text: '@TODO nom'
+          },
+          xAxis: {
+            type: 'linear',
+            min: 0,
+            max: 10,
+            tickPixelInterval: 150
+          },
+          yAxis: {
+            title: {
+              text: 'Noise'
+            },
+            plotLines: [{
+              value: 0,
+              width: 1,
+              color: '#808080'
+            }]
+          },
+          legend: {
+            enabled: false
+          },
+          exporting: {
+            enabled: false
+          },
+          credits: false,
+          series: [{
+            data: []
+          }]
+        });
         
         return this.$el;
       },
